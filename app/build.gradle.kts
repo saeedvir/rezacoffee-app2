58f0b570b1eb73d47e853ea9dcdbb2bdd261505a
saeedvir <saeed.es91@gmail.com>
2026-09-20 09:32:14 +0330

    update build kit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,6 +25,10 @@ android {
             storePassword = System.getenv("KEYSTORE_PASSWORD") ?: ""
             keyAlias = System.getenv("KEY_ALIAS") ?: ""
             keyPassword = System.getenv("KEY_PASSWORD") ?: ""
+			enableV1Signing = true
+            enableV2Signing = true
+            enableV3Signing = true
+            enableV4Signing = true
         }
     }
 
